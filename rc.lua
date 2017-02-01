fe6c5bb335cbc06f692526857ec613056c6d597d
Lauri Niskanen <ape@ape3000.com>
2017-02-01 17:42:54 +0200

    Focus windows also on right and middle click

--- rc.lua
+++ rc.lua
@@ -343,6 +343,8 @@ end
 
 clientbuttons = awful.util.table.join(
     awful.button({}, 1, function (c) client.focus = c; c:raise() end),
+    awful.button({}, 2, function (c) client.focus = c; c:raise() end),
+    awful.button({}, 3, function (c) client.focus = c; c:raise() end),
     awful.button({ modkey }, 1, awful.mouse.client.move),
     awful.button({ modkey }, 3, awful.mouse.client.resize)
 )
